495b7b7c8202c55213f292be5a51a9800af26f65
Marketa Novackova <m.kopecna@centrum.cz>
2016-10-27 14:27:25 +0200

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+#propojeni modelu a sablon
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+'''
+python manage.py shell
+
+from blog.models import Post
+Post.objects.all()
+Post.objects.all()[0].text
+
+'''
